2d3abf7d2e4712c4cddee4bd9a1df0b20d916dcd
Steve Marshall <stevesonoa@gmail.com>
2020-03-21 21:06:24 -0700

    Updated round numbers in scores area.

--- src/PlayerInfo.js
+++ src/PlayerInfo.js
@@ -1,33 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const PlayerInfo = ({handleChange, name, nameSize, scores}) => (
-    <div className='player-info-container'>
-        <div id='player-name' style={{fontSize: `${nameSize}px`}}>
+const PlayerInfo = ({ name, nameSize, scores }) => (
+    <div className="player-info-container">
+        <div id="player-name" style={{ fontSize: `${nameSize}px` }}>
             {name}
         </div>
-        <div id='player-scores'>
-            <div>{isNaN(scores[0]) ? '' : scores[0]}</div>
-            <div>{isNaN(scores[1]) ? '' : scores[1]}</div>
-            <div>{isNaN(scores[2]) ? '' : scores[2]}</div>
+        <div id="player-scores">
+            <div data-round-number="1">{isNaN(scores[0]) ? "" : scores[0]}</div>
+            <div data-round-number="2">{isNaN(scores[1]) ? "" : scores[1]}</div>
+            <div data-round-number="3">{isNaN(scores[2]) ? "" : scores[2]}</div>
         </div>
-        <div id='total-score'>
+        <div id="total-score">
             {isNaN(scores[0])
-                ? ''
+                ? ""
                 : scores.reduce((score, total) => score + total, 0)}
         </div>
     </div>
 );
 
 PlayerInfo.propTypes = {
-    handleChange: PropTypes.func.isRequired,
     name: PropTypes.string.isRequired,
     nameSize: PropTypes.number,
-    scores: PropTypes.arrayOf(PropTypes.number).isRequired,
+    scores: PropTypes.arrayOf(PropTypes.number).isRequired
 };
 
 PlayerInfo.defaultProps = {
-    nameSize: 120,
+    nameSize: 120
 };
 
 export default PlayerInfo;
